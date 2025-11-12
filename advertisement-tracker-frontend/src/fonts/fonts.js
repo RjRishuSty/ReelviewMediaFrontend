fab0a1ab5244a0337baa90a3ae51e080b8c5b701
RjRishuSty <rishurajk59@gmail.com>
2025-11-12 16:04:45 +0530

    delete frontend

--- advertisement-tracker-frontend/src/fonts/fonts.js
+++
@@ -1,9 +0,0 @@
-// Poppins for  (Headings)
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/poppins/700.css";
-
-// Inter for description. (Body text)
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
