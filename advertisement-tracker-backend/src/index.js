2b8b34b8722c1fe828af259eca6cd5838fe57d53
RjRishuSty <rishurajk59@gmail.com>
2025-11-17 17:48:58 +0530

    done

--- advertisement-tracker-backend/src/index.js
+++ advertisement-tracker-backend/src/index.js
@@ -32,7 +32,7 @@ app.use(
     cookie: {
       httpOnly:true,
       sameSite:'lax',
-      secure: false,
+      secure:  process.env.APP_MODE === "production",
       maxAge: 24 * 60 * 60 * 1000,
     }, // 1 day
   })
