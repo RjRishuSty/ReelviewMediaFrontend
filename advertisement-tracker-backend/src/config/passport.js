944f5c6083b54cebeb03a9027f4eecccf5d8c713
RjRishuSty <rishurajk59@gmail.com>
2025-11-18 15:53:35 +0530

    Change template liters

--- advertisement-tracker-backend/src/config/passport.js
+++ advertisement-tracker-backend/src/config/passport.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 passport.use(
   new GoogleStrategy(
-    {
+    { 
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/v1/auth/google/callback`,
+      callbackURL: process.env.BACKEND_URL + '/api/v1/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
